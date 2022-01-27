6cc42bec16e0751244df89f65d7f29b3077679d6
shreyansh <shreyanshagra2310@gmail.com>
2022-01-27 11:18:29 +0530

    removed the right click menu

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -127,7 +127,6 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
